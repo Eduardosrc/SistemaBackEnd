fc60ebdfb04ddc598c75b1c0d0cae7d8f48795da
Eduardosrc <eduardosrc37@gmail.com>
2023-05-30 20:11:25 -0500

    SubidaDeProyecto

---
+++ src/main/java/com/theo/SistemaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.theo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaBackEndApplication.class, args);
+    }
+
+}
