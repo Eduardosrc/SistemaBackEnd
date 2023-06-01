e098022e7a547baf7d20ae5ad9d714259a04318b
Eduardosrc <eduardosrc37@gmail.com>
2023-05-31 20:02:51 -0500

    add especialidadControllerCRUD

--- src/main/java/com/theo/controller/EspecialidadController.java
+++ src/main/java/com/theo/controller/EspecialidadController.java
@@ -1,14 +1,15 @@
 package com.theo.controller;
 
+import com.theo.dto.especialidad.EspecialidadCreateDto;
 import com.theo.dto.especialidad.EspecialidadDto;
+import com.theo.dto.especialidad.EspecialidadUpdateDto;
 import com.theo.service.EspecialidadService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,6 +22,31 @@ public class EspecialidadController {
     @GetMapping
     public ResponseEntity<List<EspecialidadDto>> listar(){
         return new ResponseEntity<>(service.listarTodas(), HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<EspecialidadDto> buscarPorId(@PathVariable("id") Long id){
+        EspecialidadDto especialidadDto = service.buscarPorId(id);
+        if(especialidadDto == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else{
+            return new ResponseEntity<>(especialidadDto, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<EspecialidadDto> registrar(@Valid @RequestBody EspecialidadCreateDto especialidadCreateDto){
+        return new ResponseEntity<>(service.registrar(especialidadCreateDto), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<EspecialidadDto> actualizar(@Valid @RequestBody EspecialidadUpdateDto especialidadUpdateDto){
+        EspecialidadDto especialidadDto = service.buscarPorId(especialidadUpdateDto.getId());
+        if(especialidadDto == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else{
+            return new ResponseEntity<>(service.actualizar(especialidadUpdateDto), HttpStatus.OK);
+        }
     }
 
 }
